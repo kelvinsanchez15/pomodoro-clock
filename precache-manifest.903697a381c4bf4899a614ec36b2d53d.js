7cd2cb078bc3b9963db5ab4ca53119fc667a4356
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-07-19 08:32:22 -0400

    Updates

---
+++ precache-manifest.903697a381c4bf4899a614ec36b2d53d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a493a48af2f630e77fef3005141583",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "c9ecadf20f71375ae6e9",
+    "url": "/pomodoro-clock/static/css/main.bbecde6e.chunk.css"
+  },
+  {
+    "revision": "4dfe0c8a4d0ed152dcb0",
+    "url": "/pomodoro-clock/static/js/2.5ca0f28d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.5ca0f28d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9ecadf20f71375ae6e9",
+    "url": "/pomodoro-clock/static/js/main.41f791fc.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  }
+]);
